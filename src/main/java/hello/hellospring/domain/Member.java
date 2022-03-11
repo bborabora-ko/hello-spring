4e634ebf4e807252b35932d5414f70f2ae36e83c
bborabora-ko <bborabora.ko@gmail.com>
2022-03-12 00:10:34 +0900

    section6-5. jpa

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity //jpa가 관리하는 entity가 됨
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //pk연결, db가 알아서 생성하는것=Identity
     private Long id;
+
     private String name;
 
     public Long getId() {
