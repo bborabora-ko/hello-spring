d9798970b709fa533b2150601ebe3428854e4dd1
bborabora-ko <bborabora.ko@gmail.com>
2022-03-11 22:38:19 +0900

    section6-3. test //spring integration test

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,38 +1,33 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//단위테스트 = 순수한 자바코드로 하면서 최소한의 단위로 테스트 하는 것
-// -> 가급적 이 방식으로 해야 좋은 테스트라 할 수 있음
-class MemberServiceTest {
+@SpringBootTest //스프링 컨테이너와 테스트를 함께 실행
+@Transactional
+//테스트 케이스에 이 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고,
+//테스트 완료 후에 항상 롤백한다.(테스트 건건마다)
+//이렇게 하면 DB에 데이터가 남지 않으므로 다음 테스트에 영향을 주지 않는다.
+class MemberServiceIntegrationTest { //통합테스트-자바코드와 스프링 다같이 테스트 하는 것
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach(){
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach(){
-        memberRepository.clearStore(); //이제 메소드가 끝날때마다 store가 비워지게 됨
-    }
+   @Autowired MemberService memberService;
+   @Autowired MemberRepository memberRepository;
 
     @Test
     void 회원가입() {
         //given -> 이런 상황이 주어졌을 때(=이 데이터를 기반으로 하는구나!)
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring1");
 
         //when -> 이거를 실행했을 때(=이걸 검증하고 싶구나!)
         Long saveId = memberService.join(member);
@@ -45,7 +40,7 @@ class MemberServiceTest {
 
     //정상코드인지 테스트하는 것도 중요하지만, 예외발생시 예외상황이 터지는지 확인하는것도 중요하다.
     @Test
-    public void 중복_회원_예외(){
+    public void 중복_회원_예외() {
         //given
         Member member1 = new Member();
         member1.setName("spring");
@@ -58,23 +53,5 @@ class MemberServiceTest {
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
-       /* //when
-        memberService.join(member1);
-        try{
-            memberService.join(member2);
-            fail();
-        }catch (IllegalStateException e){
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123123");
-        }*/
-        //then
-    }
-
-
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
     }
 }
