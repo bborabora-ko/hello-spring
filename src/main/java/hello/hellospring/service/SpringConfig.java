cf06d86ad8b2667db4f91b8c9e89e224e02e10aa
bborabora-ko <bborabora.ko@gmail.com>
2022-03-12 00:27:17 +0900

    section6-6. spring data jpa

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -19,23 +19,30 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean //@Bean = 스프링빈에 등록해!
     public MemberService memberservice(){
-        return new MemberService(memberRepository()); //스프링빈에 있는 memberRepository()를 엮어줘야하기 때문에 파라미터로 넣어줌
+        return new MemberService(memberRepository); //스프링빈에 있는 memberRepository()를 엮어줘야하기 때문에 파라미터로 넣어줌
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 
 }
